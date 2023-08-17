7a999c4dfc37b11465d128c17593d1b79b436c47
ehuard <ehuard.eh@gmail.com>
2023-08-17 19:04:33 -0400

    fix fond de carte

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Données en provenance de https://sql.sh/736-base-donnees-villes-francaises
 J'ai ajouté le titre des colonnes
+
+
+Aussi, fond de carte provenant de https://www.data.gouv.fr/fr/datasets/contours-des-communes-de-france-simplifie-avec-regions-et-departement-doutre-mer-rapproches/
+J'ai enlevé à la main les DROM rapprochés
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable dot-notation */
 'use strict'
 
 import * as map from './scripts/map.js'
@@ -5,46 +6,46 @@ import * as preprocess from './scripts/preprocess.js'
 import * as legend from './scripts/legend.js'
 import * as hover from './scripts/hover.js'
 
-
+/**
+ * Gives the canvas the right size  and create a container for the map background
+ * @param {number} width The data to be displayed
+ * @param {number} height The projection to use to convert the longitude and latitude
+ */
 function generateMapG (width, height) {
-    // canvas
-    d3.select('.map')
-      .attr('width', width)
-      .attr('height', height)
-      // fond de carte
-      .append("g")
-        .attr("class","fond")
-        .attr('width', width)
-        .attr('height', height)
-  
+  // canvas
+  d3.select('.map')
+    .attr('width', width)
+    .attr('height', height)
+    // fond de carte
+    .append('g')
+    .attr('class', 'fond')
+    .attr('width', width)
+    .attr('height', height)
 }
 
 (function (d3) {
-    const svgSize = {
-      width: 800,
-      height: 625
-    }
-    generateMapG(svgSize["width"], svgSize["height"])
+  const svgSize = {
+    width: 800,
+    height: 625
+  }
+  generateMapG(svgSize['width'], svgSize['height'])
 
-    const projection = d3.geoConicConformal()
+  const projection = d3.geoEqualEarth()
     .center([2.454071, 46.279229]) // Center of France
-    .scale(2600) // Adjust scale as needed
-    .translate([svgSize["width"] / 2, svgSize["height"] / 2]);
+    .scale(3000) // Adjust scale as needed
+    .translate([svgSize['width'] / 2, svgSize['height'] / 2])
 
-    const path = d3.geoPath().projection(projection);
-
-    // Load GeoJSON data
-    d3.json("data/fra2021.json").then(function(data) {
+  const path = d3.geoPath().projection(projection)
+  // Load GeoJSON data
+  d3.json('./fra2021.json', d3.autoType).then(function (data) {
     // Create a path for each feature in the GeoJSON
     console.log(data)
-    svg.selectAll(".path")
-    .data(data.features)
-    .enter()
-    .append("path")
-    .attr("d", path)
-    .attr("stroke", "black")
-    .attr("fill", "lightblue"); // Adjust fill color as needed
-});
-  
-    console.log("salut")
-})(d3);
+    d3.select('.fond').selectAll('.path')
+      .data(data.features)
+      .enter()
+      .append('path')
+      .attr('d', path)
+      .attr('stroke', 'black')
+      .attr('fill', 'lightblue') // Adjust fill color as needed
+  })
+})(d3)
