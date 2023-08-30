4df093f147c1f9912e9e5bcbbd5cf236a6f61d09
ehuard <ehuard.eh@gmail.com>
2023-08-30 19:36:07 -0400

    correct comment and doc

--- src/scripts/hover.js
+++ src/scripts/hover.js
@@ -1,5 +1,10 @@
+/**
+ * Set the comportment of the elements in the legend when hovered
+ * When we hover the circle, all the markers associated with this suffix will grow
+ * from 2px to 4px of radius
+ */
 export function setLegendMarkerHandler () {
-  // Select the squares and set their event handlers
+  // Select the legend circles and set their event handlers
   const circles = d3.selectAll('.legend-circle')
   circles.on('mouseover', function () {
     const parentDivId = d3.select(this.parentNode.parentNode).attr('id')
